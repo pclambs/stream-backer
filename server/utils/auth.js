6866592d89d7a66ed10b3392b1afceeb8b6dd472
Max <maxrrice@hotmail.com>
2023-11-07 18:36:00 -0600

    added missing auth middleware

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -14,4 +14,24 @@ module.exports = {
     const payload = { email, name, _id };
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
+  authMiddleware: function ({ req }) {
+    let token = req.body.token || req.query.token || req.headers.authorization;
+
+    if (req.headers.authorization) {
+      token = token.split(' ').pop().trim();
+    }
+
+    if (!token) {
+      return req;
+    }
+
+    try {
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch {
+      console.log('Invalid token');
+    }
+
+    return req;
+  },
 };

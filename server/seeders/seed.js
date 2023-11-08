aa787538824e32e00a84bcd3a6837c457b7ba10b
deepwubs <pchucklambs@gmail.com>
2023-11-07 21:38:55 -0600

    added seeds

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,17 +1,23 @@
-const db = require('../config/connection');
-const { Profile } = require('../models');
-const profileSeeds = require('./profileSeeds.json');
-const cleanDB = require('./cleanDB');
+const db = require('../config/connection')
+const { Profile } = require('../models')
+const profileSeeds = require('./profileSeeds.json')
+const videoPostSeeds = require('./videoPostSeeds.json')
+const commentSeeds = require('./commentSeeds.json')
+const cleanDB = require('./cleanDB')
 
 db.once('open', async () => {
   try {
-    await cleanDB('Profile', 'profiles');
+    await cleanDB('Profile', 'profiles')
+    await cleanDB('VideoPost', 'videoposts')
+    await cleanDB('Comment', 'comments')
     
     await Profile.create(profileSeeds);
+    await Profile.create(videoPostSeeds);
+    await Profile.create(commentSeeds);
 
-    console.log('all done!');
-    process.exit(0);
+    console.log('all done!')
+    process.exit(0)
   } catch (err) {
-    throw err;
+    throw err
   }
-});
+})

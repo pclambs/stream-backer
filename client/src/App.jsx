8db7415393e7fed4548139fdb1581a6ac0e42fa7
deepwubs <pchucklambs@gmail.com>
2023-11-10 01:30:39 -0600

    setup small testUpload mutation for cloudinary

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,14 @@
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
-} from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
-import { Outlet } from 'react-router-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
+import { Outlet } from 'react-router-dom'
 
-import Header from './components/Header/';
-import Footer from './components/Footer';
+import Header from './components/Header/'
+import Footer from './components/Footer'
 
 // Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
   uri: '/graphql',
-});
+})
 
 // Construct request middleware that will attach the JWT token to every request as an `authorization` header
 const authLink = setContext((_, { headers }) => {
--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express');
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
@@ -12,27 +13,27 @@ const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-});
+})
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async () => {
   await server.start();
-
+  
   app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
-
+  
   app.use('/graphql', expressMiddleware(server, {
     context: authMiddleware
   }));
-
+  
   if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/dist')));
-
+    
     app.get('*', (req, res) => {
       res.sendFile(path.join(__dirname, '../client/dist/index.html'));
     });
   }
-
+  
   db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
@@ -42,4 +43,4 @@ const startApolloServer = async () => {
 };
 
 // Call the async function to start the server
-  startApolloServer();
+startApolloServer();

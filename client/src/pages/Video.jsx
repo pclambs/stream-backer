e9b306879c9f1ec234052c976b02ca8be6baf70d
Bakeradm6 <123577761+Bakeradm6@users.noreply.github.com>
2023-11-13 10:14:12 -0600

    added base code to Video page

--- client/src/pages/Video.jsx
+++ client/src/pages/Video.jsx
@@ -1,10 +1,15 @@
 import VideoPlayer from "../components/VideoPlayer"
-
+import CommentContainer from "../components/CommentContainer"
 
 
 const Video = () => {
+
+  // TODO add comment variable
+  const comments = ''
+
   return <div>
     <VideoPlayer />
+    <CommentContainer comments={comments} />
 
   </div>;
 };

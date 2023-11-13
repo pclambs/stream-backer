2bcf79f7c61d7232dc983a403e465a8becc0878c
Bakeradm6 <123577761+Bakeradm6@users.noreply.github.com>
2023-11-13 11:55:43 -0600

    added useParams to Video page

--- client/src/pages/Video.jsx
+++ client/src/pages/Video.jsx
@@ -1,5 +1,6 @@
 import {useState, useEffect} from 'react'
 import {useQuery} from '@apollo/client'
+import {useLocation, useParams} from 'react-router-dom'
 import {QUERY_SINGLE_VIDEOPOST} from '../utils/queries'
 import VideoPlayer from "../components/VideoPlayer"
 import CommentContainer from "../components/CommentContainer"
@@ -10,22 +11,24 @@ import CommentContainer from "../components/CommentContainer"
 const Video = () => {
 
   // TODO add comment variable
-  const [comments, setComments] = useState([])
-  const {loading, error, data} = useQuery(QUERY_SINGLE_VIDEOPOST)
+    const [comments, setComments] = useState([])
+    const location = useLocation()
+    const {videoPostId} = useParams()
+    const {loading, error, data} = useQuery(QUERY_SINGLE_VIDEOPOST, {variables: {videoPostId}})
   
 
   useEffect(() => {
-    if (data && data.comments) {
-      setComments(data.comments)
+    if (data && data.videoPost.comments) {
+      setComments(data.videoPost.comments)
     }
   }, [data])
 
   return (
     <div>
-    {error && <p>{error}</p>}
+    {error && <p>{error.message}</p>}
     {loading && <p>Loading...</p>}
-    <VideoPlayer />
-    <CommentContainer comments={data.comments} />
+    <VideoPlayer videoData={data?.videoPost}/>
+    <CommentContainer comments={comments} />
   </div>
   )
 }

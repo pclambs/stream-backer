b47ccd2b900a52f7e7e1717baaa7b0fbfeffa009
Bakeradm6 <123577761+Bakeradm6@users.noreply.github.com>
2023-11-13 10:08:26 -0600

    added base code to CommentCard

--- client/src/components/CommentCard.jsx
+++ client/src/components/CommentCard.jsx
@@ -2,3 +2,14 @@
 
 import React from 'react'
 
+const CommentCard = ({comment}) => {
+    return (
+        <div className="card comment-card">
+            <h3>{comment.commentBody}</h3>
+            <h4>{comment.postedBy.username}</h4>
+            <p>{comment.dateCreated}</p>
+        </div>
+    )
+}
+
+export default CommentCard

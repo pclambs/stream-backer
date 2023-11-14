c83f712c4c22c3e0bacb170d641d5aaee17244b2
deepwubs <pchucklambs@gmail.com>
2023-11-14 13:51:42 -0600

    added start of upload page

--- client/src/pages/Upload.jsx
+++ client/src/pages/Upload.jsx
@@ -1,10 +1,77 @@
+import React, { useState } from 'react'
+import { Button, Typography, Container, Paper } from '@mui/material'
+import CustomTextField from '../components/CustomTextField'
+import CloudUploadIcon from '@mui/icons-material/CloudUpload'
+
+
 //Page to upload a new video or edit existing one
 const Upload = () => {
-    return (
-        <div>
-            <h1>Beep Boop</h1>
-        </div>
-    )
+	const [formState, setFormState] = useState({
+		title: '',
+    description: '',
+    tags: '',
+  });
+
+	const handleChange = (event) => {
+		const { name, value } = event.target
+	
+		setFormState({
+			...formState,
+			[name]: value,
+		})
+	}
+	
+	return (
+		<Container disableGutters sx={{marginY: 1.2}}>
+			<Paper>
+				<Typography variant="h3" sx={{}}>Upload a New Video</Typography>
+					<form>
+						<CustomTextField
+							name="title"
+							type="tet"
+							value={formState.title}
+							onChange={handleChange}
+							variant='standard'
+							label='Title'
+							margin='normal'
+						/>
+						<CustomTextField
+							name="description"
+							type="text"
+							value={formState.description}
+							onChange={handleChange}
+							variant='standard'
+							rows={4}
+							multiline
+							label='Description'
+							margin='normal'
+						/>
+						<CustomTextField
+							name="tags"
+							type="text"
+							value={formState.tags}
+							onChange={handleChange}
+							variant='standard'
+							label='Tags'
+							margin='normal'
+						/>
+						<Button 
+							variant="contained" 
+							color="primary" 
+							type="submit" 
+							size='large'
+							sx={{
+								width: '100%',
+								marginTop: '1rem'
+							}}
+							startIcon={<CloudUploadIcon />}
+						>
+							Upload
+						</Button>
+					</form>
+				</Paper>
+		</Container>
+	)
 }
 
 export default Upload

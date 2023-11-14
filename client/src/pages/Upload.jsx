d6be9ad518fd24dd358b30268d9151beecc63df4
deepwubs <pchucklambs@gmail.com>
2023-11-14 03:08:13 -0600

    added routes anc connected new pages

--- client/src/pages/Upload.jsx
+++ client/src/pages/Upload.jsx
@@ -1,5 +1,13 @@
 //Page to upload a new video or edit existing one
+const Upload = () => {
+    return (
+        <div>
+            <h1>Beep Boop</h1>
+        </div>
+    )
+}
 
+export default Upload
     //Must be logged in or be redirected to login page
 
     //If editing, pull in current object values and resave object to db

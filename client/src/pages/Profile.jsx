572e0e6ac94dd28c6fa21d8bf1d1f0f7bd20cb47
Lindsay <lshank017@gmail.com>
2023-11-15 15:23:08 -0600

    fixed code error

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,37 +1,37 @@
-import { useParams } from 'react-router-dom';
-import { useQuery, useMutation } from '@apollo/client';
-import auth from '../utils/auth';
-import { QUERY_SINGLE_PROFILE } from '../utils/queries';
-import Auth from '../utils/auth';
-import { UPDATE_PROFILE } from '../utils/mutations';
-import UserForm from '../components/UserForm';
+import { useParams } from "react-router-dom";
+import { useQuery, useMutation } from "@apollo/client";
+import auth from "../utils/auth";
+import { QUERY_SINGLE_PROFILE } from "../utils/queries";
+import { UPDATE_PROFILE } from "../utils/mutations";
+import UserForm from "../components/UserForm";
+// import ProfileCard from "../components/ProfileCard";
 
 const Profile = () => {
-  const loggedInUserId = Auth.loggedIn()
-  const { profileId } = useParams();
-  console.log(profileId)
-  const { loading, error, data } = useQuery(QUERY_SINGLE_PROFILE, {
-    variables: { profileId },
+  const loggedInUserId = auth.getProfile()?.data?._id;
+
+  const { loading, data } = useQuery(QUERY_SINGLE_PROFILE, {
+    variables: { profileId: loggedInUserId },
   });
-  
-  console.log('Data', data)
+
   const profile = data?.profile || {};
-  const [updateUser] = useMutation(UPDATE_PROFILE)
-  console.log('profile', profile);
+  const [updateUser] = useMutation(UPDATE_PROFILE);
+  console.log("profile", profile);
 
   if (loading) {
     return <div>Loading...</div>;
   }
   return (
     <div>
-      <h2>
-       Welcome {profile.username} !
-      </h2>
+      <h2>Welcome {profile.username}!</h2>
       {/* <ProfileCard /> */}
-      <UserForm initialValue={
-        {...profile, profileId: loggedInUserId}} onSubmit={(userBody)=> updateUser({
-        variables: userBody
-      })}/>
+      <UserForm
+        initialValue={{ ...profile, profileId: loggedInUserId }}
+        onSubmit={(userBody) =>
+          updateUser({
+            variables: userBody,
+          })
+        }
+      />
     </div>
   );
 };

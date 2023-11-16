941cca5051daaa8ab0c870d476dccd72c0f02bbc
deepwubs <pchucklambs@gmail.com>
2023-11-16 01:31:12 -0600

    stopped auto open of vite server

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    open: true,
+    // open: true,
     proxy: {
       '/graphql': {
         target: 'http://localhost:3001',
